6fa40fe4efb9195234b69a120714d3c76de34b95
andreister <andreister@bd649bb6-e030-11dd-ab27-75d9513d8d6c>
2009-02-18 19:22:37 +0000

    Add CS tests and stubs for VB.

---
+++ trunk/Tests/CS/MoqTests/BrainTests.cs
@@ -0,0 +1,39 @@
+﻿using MockingFrameworksCompare.BrainSample;
+using Moq;
+using NUnit.Framework;
+
+namespace MoqTests
+{
+    [TestFixture]
+    public class BrainTests
+    {
+        /// <summary>
+        /// Verify that if hand throws an exception having touched a hot iron,  <see cref="IMouth.Yell"/> gets called.
+        /// </summary>
+        /// <remarks>
+        /// Moq can mock both interfaces and classes - however, only virtual methods 
+        /// of a class can be mocked (try changing IHand/IMouth to Hand/Mouth).
+        /// </remarks>
+        [Test]
+        public void TouchHotIron_Yell()
+        {
+            var hand = new Mock<IHand>();
+            var mouth = new Mock<IMouth>();
+            hand.Setup(x => x.TouchIron(HotIron())).Throws(new BurnException());
+
+            var brain = new Brain(hand.Object, mouth.Object);
+            brain.TouchIron(new Iron { IsHot = true });
+
+            mouth.Verify(x => x.Yell());    
+        }
+
+        /// <summary>
+        /// Parameter expectations tend to be quite verbose in Moq, so we provide a custom matcher.
+        /// This needs a matcher method and a bool sibling method for evaluating the expectations.
+        /// Calling this matcher is technically equivalent to <code>It.Is{Iron}(i => i.IsHot)</code>.
+        /// </summary>
+        [Matcher]
+        private static Iron HotIron() { return null; }
+        public static bool HotIron(Iron iron) { return iron.IsHot; }
+    }
+}
---
+++ trunk/Tests/VB/MoqTests/BrainTests.vb
@@ -0,0 +1,3 @@
+﻿Public Class BrainTests
+
+End Class
