35ebe4526aeed423f39d042acb99e7ac19479967
andreister <andreister@bd649bb6-e030-11dd-ab27-75d9513d8d6c>
2009-03-08 19:28:25 +0000

    Add missing matcher from Moq revision #534

---
+++ Frameworks/Moq/Source/Matchers/Matcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Moq
+{
+	internal class Matcher : IMatcher
+	{
+		Match match;
+
+		public Matcher(Match match)
+		{
+			this.match = match;
+		}
+
+		public void Initialize(Expression matcherExpression)
+		{
+		}
+
+		public bool Matches(object value)
+		{
+			return match.Matches(value);
+		}
+	}
+}
