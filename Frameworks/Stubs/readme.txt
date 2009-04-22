7ef3934adcb249f53ca5c458bafce590b1a228ea
jonathan.dehalleux <jonathan.dehalleux@bd649bb6-e030-11dd-ab27-75d9513d8d6c>
2009-04-22 00:04:13 +0000

    added stubs framework examples

---
+++ Frameworks/Stubs/readme.txt
@@ -0,0 +1,1 @@
+The full version of Stubs is available for download at http://research.microsoft.com/stubs.
---
+++ Tests/CS/StubsTests/BrainTests.cs
@@ -0,0 +1,41 @@
+﻿using MockingFrameworksCompare.BrainSample;
+using NUnit.Framework;
+using MockingFrameworksCompare.BrainSample.Stubs;
+
+namespace StubsTests
+{
+    /// <summary>
+    /// Brain test implementation using Stubs.
+    /// Stubs home page: http://research.microsoft.com/stubs
+    /// </summary>
+    [TestFixture]
+    public class BrainTests
+    {
+        /// <summary>
+        /// Verify that if hand throws an exception having touched a hot iron,  <see cref="IMouth.Yell"/> gets called.
+        /// </summary>
+        /// <remarks>
+        /// Stubs can mock both interfaces and classes - however, only virtual methods 
+        /// of a class can be mocked (try changing IHand/IMouth to Hand/Mouth).
+        /// </remarks>
+        [Test]
+        public void TouchHotIron_Yell()
+        {
+            var hand = new SIHand();
+            var mouth = new SIMouth();
+
+            hand.TouchIron = (iron) =>
+            {
+                if (iron.IsHot)
+                    throw new BurnException();
+            };
+            bool yelled = false;
+            mouth.Yell = () => yelled = true;
+
+            var brain = new Brain(hand, mouth);
+            brain.TouchIron(new Iron { IsHot = true });
+
+            Assert.IsTrue(yelled);
+        }
+    }
+}
