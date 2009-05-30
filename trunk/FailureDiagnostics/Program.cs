6dc37f1abbafa3bca02be43f027b806e2774a270
andreister <andreister@bd649bb6-e030-11dd-ab27-75d9513d8d6c>
2009-05-30 22:36:47 +0000

    Implement some failures to compare error messages

--- trunk/FailureDiagnostics/Program.cs
+++ trunk/FailureDiagnostics/Program.cs
@@ -5,49 +5,93 @@ namespace FailureDiagnostics
 {
     class Program
     {
-        [DllImport("kernel32.dll")]
-        public static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, int wAttributes);
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetStdHandle(uint nStdHandle);
-
         static void Main(string[] args)
         {
             var moq = new MoqTests.FailingTests();
             var rhino = new RhinoMocksTests.FailingTests();
             var nmock2 = new NMock2Tests.FailingTests();
             var isolator = new IsolatorTests.FailingTests();
-            //var stubs = new StubsTests.ShoppingCartTests();
+            var stubs = new StubsTests.ShoppingCartTests();
 
             Console.WriteLine("Case 1. Some unexpected method gets invoked.");
-            Run("Moq      ", () => moq.CallYell_Unexpected());
-            Run("Rhino    ", () => rhino.CallYell_Unexpected());
-            Run("NMock2   ", () => nmock2.CallYell_Unexpected());
-            Run("Isolator ", () => isolator.CallYell_Unexpected());
+            Run("Moq      ", moq.CallOnceExpectNever);
+            Run("Rhino    ", rhino.CallOnceExpectNever);
+            Run("NMock2   ", nmock2.CallOnceExpectNever);
+            Run("Isolator ", isolator.CallOnceExpectNever);
+            Console.WriteLine("Press any key to continue..."); Console.ReadLine();
+
+            Console.WriteLine("Case 2. Some expected method is not invoked.");
+            Run("Moq      ", moq.CallNeverExpectOnce);
+            Run("Rhino    ", rhino.CallNeverExpectOnce);
+            Run("NMock2   ", nmock2.CallNeverExpectOnce);
+            Run("Isolator ", isolator.CallNeverExpectOnce);
+            Console.WriteLine("Press any key to continue..."); Console.ReadLine();
+
+            Console.WriteLine("Case 3. Some expected method is not invoked - custom message.");
+            Run("Moq      ", moq.CallNeverExpectOnceCustom);
+            Run("Rhino    ", rhino.CallNeverExpectOnceCustom);
+            Run("NMock2   ", nmock2.CallNeverExpectOnceCustom);
+            Run("Isolator ", isolator.CallNeverExpectOnceCustom);
+        }
+
+        #region Console colored output
+
+        [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetStdHandle(uint nStdHandle);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetConsoleScreenBufferInfo", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetConsoleScreenBufferInfo(int hConsoleOutput, ref CONSOLE_SCREEN_BUFFER_INFO lpConsoleScreenBufferInfo);
+        
+        [DllImport("kernel32.dll", EntryPoint = "SetConsoleTextAttribute", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetConsoleTextAttribute(int hConsoleOutput, int wAttributes);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        private struct COORD { short X; short Y; }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SMALL_RECT { short Left; short Top; short Right; short Bottom; }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct CONSOLE_SCREEN_BUFFER_INFO
+        {
+            public COORD dwSize;
+            public COORD dwCursorPosition;
+            public int wAttributes;
+            public SMALL_RECT srWindow;
+            public COORD dwMaximumWindowSize;
         }
 
         private static void Run(string text, Action action)
         {
-            const uint STD_OUTPUT_HANDLE = 0xfffffff5;
+            const uint stdOutputHandle = 0xfffffff5;
             const int green = 2;
             const int red = 12;
-            const int white = 7;// 15;
 
-            var console = GetStdHandle(STD_OUTPUT_HANDLE);
-
+            var console = GetStdHandle(stdOutputHandle);
+            int originalColor = GetOriginalColor(console);
+            
             try {
                 SetConsoleTextAttribute(console, green);
                 Console.WriteLine(text);
                 action();
             }
-            catch(Exception ex) {
+            catch (Exception ex) {
                 SetConsoleTextAttribute(console, red);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine();
             }
             finally {
-                SetConsoleTextAttribute(console, white);
+                SetConsoleTextAttribute(console, originalColor);
             }
         }
+ 
+        private static int GetOriginalColor(int hConsole)
+        {
+            var consoleInfo = new CONSOLE_SCREEN_BUFFER_INFO();
+            GetConsoleScreenBufferInfo(hConsole, ref consoleInfo);
+            return consoleInfo.wAttributes;
+        }
 
+        #endregion
     }
 }
